d23bbe24dc899fd03c63f68e46a5ae80d99d0b96
Sam331 <36762547+zyhusesit@users.noreply.github.com>
2022-03-08 20:42:19 +0800

    link B is correct now!

--- main.cpp
+++ main.cpp
@@ -8,10 +8,12 @@ typedef struct node {
     struct node *next;
 } List;
 
-void getlist(List *A, int n);//创建包含N个随机数的链表
-void showlist(List *A);//显示链表内容
-void dellist(List *A);//销毁链表
-void insdata(List *A, int x);//有序链表中插入包含数据x的节点
+void getlistA(List *A, int n);//创建包含N个随机数的链表
+void getlistB(List *B, int n);//创建包含N个随机数的链表
+void showList(List *A);//显示链表内容
+void deleteList(List *A);//销毁链表
+void insertData(List *A, int x);//有序链表中插入包含数据x的节点
+void insertData_reverse(List *A, int x);//有序链表中插入包含数据x的节点
 void mergelist(List *A, List *B);//合并A与B，结果存于A中，使得A逆向有序且无重复元素。
 
 int main() {
@@ -20,43 +22,51 @@ int main() {
     A = new List;
     B = new List;
     A->next = B->next = nullptr;
-    cout << "请输入链表A和B的数据规模n：" << endl;
+    cout << "请输入链表A的数据规模n：" << endl;
     cin >> n;
-    cout << "请输入链表A和B的数据规模m：" << endl;
+    cout << "请输入链表B的数据规模m：" << endl;
     cin >> m;
 
     while (n > 0 && m > 0) {
-        getlist(A, n);//创建链表A
-        getlist(B, m);//创建链表B
+        getlistA(A, n);//创建链表A
+        getlistB(B, m);//创建链表B
         cout << "链表A:" << endl;
-        showlist(A);//显示A的源数据
+        showList(A);//显示A的源数据
         cout << "链表B:" << endl;
-        showlist(B);//显示B的源数据
+        showList(B);//显示B的源数据
 
         cout << "将链表A与B合并后：";
 
         mergelist(A, B);//将A与B合并，结果存于A，A逆向有序
         cout << "链表A:" << endl;
-        showlist(A);//显示处理后的链表A
+        showList(A);//显示处理后的链表A
 
-        dellist(A);//销毁链表A。此时不用销毁B，因为上述合并结束后，B已经为空
-        cout << "请输入链表A和B的数据规模n：" << endl;
+        deleteList(A);//销毁链表A。此时不用销毁B，因为上述合并结束后，B已经为空
+        cout << "请输入链表A的数据规模n：" << endl;
         cin >> n;
-        cout << "请输入链表A和B的数据规模m：" << endl;
+        cout << "请输入链表B的数据规模m：" << endl;
         cin >> m;
 
     }
 }
 
-void getlist(List *A, int n) {
+void getlistA(List *A, int n) {
     int x, i;
     for (i = 0; i < n; i++) {
         x = rand() % 31;
-        insdata(A, x);
+        insertData(A, x);
     }
 }
 
-void showlist(List *A) {
+void getlistB(List *B, int n) {
+    int x, i;
+    for (i = 0; i < n; i++) {
+        x = rand() % 31;
+        insertData_reverse(B, x);
+    }
+}
+
+void showList(List *A) {
     List *p = A->next;
     if (!p) {
         cout << "链表为空" << endl;
@@ -69,7 +79,7 @@ void showlist(List *A) {
     printf("\n");
 }
 
-void dellist(List *A) {
+void deleteList(List *A) {
     List *p, *p2;
     p = A->next;
     A->next = nullptr;
@@ -80,24 +90,44 @@ void dellist(List *A) {
     }
 }
 
-void insdata(List *A, int x) {
-    List *p, *p1, *q;
+void insertData(List *A, int x) {
+    List *p2, *p1, *q;
     p1 = A;
-    p = p1->next;
-    while (p) {
-        if (p->data > x)
+    p2 = p1->next;
+    while (p2) {
+        if (p2->data > x)
             break;
-        else if (p->data == x)
+        else if (p2->data == x)
             return;
         else {
-            p1 = p;
-            p = p->next;
+            p1 = p2;
+            p2 = p2->next;
         }
     }
     q = new List;
     q->data = x;
     p1->next = q;
-    q->next = p;
+    q->next = p2;
+}
+
+void insertData_reverse(List *A, int x) {
+    List *p2, *p1, *q;
+    p1 = A;
+    p2 = p1->next;
+    while (p2) {
+        if (p2->data < x)
+            break;
+        else if (p2->data == x)
+            return;
+        else {
+            p1 = p2;
+            p2 = p2->next;
+        }
+    }
+    q = new List;
+    q->data = x;
+    p1->next = q;
+    q->next = p2;
 }
 
 void mergelist(List *A, List *B) {
